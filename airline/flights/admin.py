0601a17504047f5e601966929eef17857a64bb9c
MYO THIRI <sandarlamin.llm@gmail.com>
2020-08-30 23:56:22 +0900

    Initial commit

---
+++ airline/flights/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Flight,Airport,Passenger
+
+# Register your models here.
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Airport)
+admin.site.register(Passenger)
+
+
